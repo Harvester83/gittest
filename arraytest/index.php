640138aee1fdc3b25ba469b2373f0a43c2ab2692
Dik83 <rashad.suleymanov83@gmail.com>
2015-01-18 15:43:44 +0400

    [WIP] добовляем кодировку  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>

--- arraytest/index.php
+++ arraytest/index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gitiest</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+</head>
+<body>
 <?php
 $EuropeMainland = array(
     'Германия' => array(
@@ -84,5 +91,6 @@ $EuropeMainland = array(
         </li>
     <?php }?>
 </ul>
-
+</body>
+</html>
 
